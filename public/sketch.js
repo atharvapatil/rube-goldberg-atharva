5c0371171bb25f71f4de6d8a780f0571ec043601
atharvapatil <atharvaabhaypatil@gmail.com>
2019-04-26 12:26:16 -0400

    Visuals updated

--- public/sketch.js
+++ public/sketch.js
@@ -10,6 +10,17 @@ let urlOn;
 let urlOff;
 let hostName = "staging.vidia.site";
 
+let bg;
+let banana;
+let flipBanana;
+
+function preload(){
+  bg = loadImage('/images/nyc-bg.png');
+  banana = loadImage('/images/banana.png');
+  flipBanana = loadImage('/images/banana-flip.png');
+}
+
+
 function urlOnSetup(){
   urlOn = "https://" + hostName + "/device-1" + "/on" ;
 }
@@ -62,16 +73,45 @@ function failureMessage(){
 }
 
 function draw() {
-  background(100);
+  background(255);
   noStroke();
 
+  image(bg,0,390,980,245);
+  image(banana,width/2 + 140, height/2 - 64, 80, 112);
+  image(flipBanana,width/2 - 220, height/2 - 64, 80, 112);
+
   if(buttonPressed == true){
-    fill(0, 220, 0);
+    fill(251,192,45);
   } else {
-    fill(220, 0, 0);
+    fill(255,214,0);
   }
 
   rectMode(CENTER);
-  rect(width/2, height/2, 200, 60);
+  rect(width/2, height/2 - 10, 240, 80);
 
+  fill(40);
+  textAlign(CENTER);
+  textSize(36);
+  textFont('Lato');
+  textStyle(BOLD);
+  text('"Banana me"', width/2, height/2);
+
+
+  textDraw();
+
+}
+
+function textDraw(){
+  //ITP NYU text
+  fill(38,50,56);
+  textAlign(CENTER);
+  textSize(88);
+  textFont('Concert One');
+  text('ITP NYU', width/2, 100);
+
+  // RUBE GOLDBERG TEXT
+  fill(55,71,79);
+  textSize(64);
+  textFont('Concert One');
+  text('Rube Goldberg Machine', width/2, 170);
 }
